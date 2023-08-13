b07c51155d15cea33b2faf53640b495046ab5986
myu-238 <118711569+myu-238@users.noreply.github.com>
2023-08-12 20:06:07 -0400

    Fixed up syntax mistakes in function formatDateAndTime

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ function formatDateAndTime(timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {
-        hours = `0$(hours)`;
+        hours = `0${hours}`;
     }
     let minutes = date.getMinutes();
     if (minutes < 10) {
-        minutes = `0$(minutes)`;
+        minutes = `0${minutes}`;
     }
     let days = [
         "Sunday",
@@ -39,11 +39,12 @@ function displayWeather(response) {
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDateAndTime(response.data.time * 1000);
-    iconElement.setAttribute(
+    /*iconElement.setAttribute(
         "src",
         `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
     );
     iconElement.setAttribute("alt", response.data.condition.description);
+    */
 }
 
 function search(city) {
