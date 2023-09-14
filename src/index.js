103c3f41d8149eb397f39cc06562a4d1422a867c
myu-238 <118711569+myu-238@users.noreply.github.com>
2023-09-13 20:16:50 -0400

    Added forecast HTML, JS, and CSS

--- src/index.js
+++ src/index.js
@@ -21,6 +21,68 @@ function formatDateAndTime(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#weather-forecast");
+
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+            forecastHTML =
+                forecastHTML +
+                `
+                            <div class="col-2">
+                                <div class="weather-forecast-date">${formatDay(
+                                    forecastDay.time
+                                )}</div>
+                                <img
+                                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                                        forecastDay.condition.icon
+                                    }.png"
+                                    alt=""
+                                    width="42"
+                                />
+                                <div class="weather-forecast-temperatures">
+                                    <span
+                                        class="weather-forecast-temperature-max"
+                                        >${Math.round(
+                                            forecastDay.temperature.maximum
+                                        )}°</span
+                                    >
+                                    <span
+                                        class="weather-forecast-temperature-min"
+                                    >
+                                        ${Math.round(
+                                            forecastDay.temperature.minimum
+                                        )}°</span
+                                    >
+                                </div>
+                            </div>
+         `;
+        }
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "28842fobf1a190b0t62a268683055905";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeather(response) {
     let temperatureElement = document.querySelector("#current-temperature");
     let cityElement = document.querySelector("#city-name");
